e121d3cc6c56bd1d41f8f1ec23273b5d16779d22
Nyimiri <nyimiri@gmail.com>
2019-12-02 19:20:52 +0300

    Initial commit

---
+++ app/src/main/java/com/example/nyimirikidsgame/MainActivity.java
@@ -0,0 +1,108 @@
+package com.example.nyimirikidsgame;
+
+import android.content.ClipData;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.DragEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+
+
+public class GamesAnimals extends AppCompatActivity {
+
+    private ImageView option, choice1, choice2, choice3;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.games_animals);
+
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setIcon(R.mipmap.ic_launcher);
+
+        //View to drag
+
+        option = (ImageView)findViewById(R.id.games_cow_item);
+
+        //Views to drop unto
+
+        choice1 = (ImageView)findViewById(R.id.games_cow_shadow);
+        choice2 = (ImageView)findViewById(R.id.games_pig_shadow);
+        choice3 = (ImageView)findViewById(R.id.games_horse_shadow);
+
+        //set touch listener
+
+        option.setOnTouchListener(new ChoiceTouchListener());
+
+        //set drag listeners
+
+        choice1.setOnDragListener(new ChoiceDragListener());
+        choice2.setOnDragListener(new ChoiceDragListener());
+        choice3.setOnDragListener(new ChoiceDragListener());
+
+
+    }
+
+    private final class ChoiceTouchListener implements View.OnTouchListener{
+        public boolean onTouch(View view, MotionEvent motionEvent){
+            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+
+                //setup drag
+                ClipData data = ClipData.newPlainText("", "");
+                View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
+
+                //start dragging the item touched
+                view.startDrag(data, shadowBuilder, view, 0);
+
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+
+
+    private class ChoiceDragListener implements View.OnDragListener{
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+            //handle drag events
+            switch (event.getAction()) {
+                case DragEvent.ACTION_DRAG_STARTED:
+                    //no action necessary
+                    break;
+                case DragEvent.ACTION_DRAG_ENTERED:
+                    //no action necessary
+                    break;
+                case DragEvent.ACTION_DRAG_EXITED:
+                    //no action necessary
+                    break;
+                case DragEvent.ACTION_DROP:
+                    //handle the dragged view being dropped over a drop view - asta de deasupra
+                    //handle the dragged view being dropped over a target view -asta de jos
+                    View view = (View) event.getLocalState();
+                    //stop displaying the view where it was before it was dragged
+                    view.setVisibility(View.INVISIBLE);
+                    //view dragged item is being dropped on
+                    ImageView dropTarget = (ImageView) v;
+                    //view being dragged and dropped
+                    ImageView dropped = (ImageView) view;
+                    //update the image in the target view to reflect the data being dropped
+                    dropTarget.setImageDrawable(dropped.getDrawable());
+
+                    break;
+                case DragEvent.ACTION_DRAG_ENDED:
+                    //no action necessary
+                    break;
+                default:
+                    break;
+            }
+
+            return true;
+        }
+    }
+}
